fdae31a42494c509f4c0ad187d735279c3bb7905
liujianyi <1300382204@qq.com>
2020-03-31 22:57:23 +0800

    create a chat room

---
+++ README.md
@@ -0,0 +1,12 @@
+# chat room
+
+> A socket-io project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# run page
+open cliten input localhost:8080 or 127.0.0.1:8080
---
+++ app.js
@@ -0,0 +1,29 @@
+var app = require('http').createServer(handler)
+var io = require('socket.io');
+
+var fs = require('fs');
+
+app.listen(8080,function(){
+    console.log("server start in 8080...")
+});
+var ws = io.listen(app);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+ws.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
